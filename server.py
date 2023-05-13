d50b7351163059b0753187db61c75c8cafa31d47
Laddge <dev.laddge@gmail.com>
2023-05-13 11:52:10 +0900

    add frontend

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import kociemba
 
 app = FastAPI()
+
+
+@app.get("/")
+async def get_root():
+    return FileResponse("./front/dist/index.html")
 
 
 @app.get("/api")
@@ -12,3 +18,13 @@ async def get_api(cubestr: str):
         print(e)
         solution = []
     return {"solution": solution}
+
+
+@app.get("/{path}")
+async def get_front(path: str):
+    return FileResponse(f"./front/dist/{path}")
+
+
+@app.get("/assets/{path}")
+async def get_assets(path: str):
+    return FileResponse(f"./front/dist/assets/{path}")
