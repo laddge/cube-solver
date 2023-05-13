dace68a4a63558ef2b030cc1e0cdca5e7e1d8c90
Laddge <dev.laddge@gmail.com>
2023-05-13 12:44:12 +0900

    solve method

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,47 +1,21 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import axios from 'axios'
+
+  let cubestr: string = ''
+  let result: string = ''
+
+  const solve = () => {
+    axios.get('/api', { params: { cubestr } })
+      .then(res => {
+        result = res.data.solution
+      })
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div class="p-6">
+  <input type="text" placeholder="Type here" bind:value={cubestr} class="input input-bordered w-full max-w-xs block mx-auto" />
+  <button on:click={solve} class="btn btn-primary w-full max-w-xs block mx-auto mt-3">Solve</button>
+  <div class="mt-3 text-center">
+    {result}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+</div>
